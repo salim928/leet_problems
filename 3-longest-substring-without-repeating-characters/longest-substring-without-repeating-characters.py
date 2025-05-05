dcbc07f832c33941d0921f9a63318abc7bb6b935
Salim Adams <salimadams49@gmail.com>
2025-05-05 21:51:43 +0000

    Time: 26 ms (48.04%) | Memory: 13.4 MB (16.78%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,11 +5,12 @@ class Solution(object):
         :rtype: int
         """
         left = 0
-        seen = {}
+        seen = set()
         max_len = 0
-        for right, char in enumerate(s):
-            if char in seen and seen[char] >= left:
-                left = seen[char] + 1
-            seen[char] = right
-            max_len = max(max_len, right - left +1 )
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+            seen.add(s[right])
+            max_len = max(max_len, right - left + 1)
         return max_len
